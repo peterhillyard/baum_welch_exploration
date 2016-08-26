bc418d2eee5af765455c64ac3c4c2def2adbf872
peterhillyard <peterhillyard@gmail.com>
2016-08-26 16:13:47 -0600

    Finished the two_state example. I discovered that estimating the mean and the variance with the MLE rather than gamma from the forward/backward algorithm did wonders! I created the n_state example to make the system generic to have n states and m observations. The same results as in the two_state example were seen here.

---
+++ n_state_example.py
@@ -0,0 +1,120 @@
+'''
+Created on Aug 26, 2016
+
+@author: pete
+'''
+import numpy as np
+
+# This script simulates the situation where we have a multi-state and a multi-
+# observation Markov model. So for example, let's say each state is a location
+# in a building, and our N observations are from N sensors. We would like to 
+# estimate the mean and variance of the multivariate gaussian distribution for
+# each state.
+
+def get_observation(mu,var):
+    normal_rand = np.random.randn(mu.size)
+    return np.sqrt(var)*normal_rand + mu
+
+N = 15 # number of states
+M = 20 # Number of observations
+
+# Set up true Markov chain parameters and the distribution parameters
+pi_true = np.ones(N)/N
+A_true = np.ones((N,N))/N
+mu_true = np.random.randint(0,10,size=(M,N))
+var_true = np.random.randint(2,7,size=(M,N))/2.
+
+# Set up our initial guess at the Markov chain and distribution parameters
+pi_est = pi_true + 0*np.random.randn(N)
+A_est  = A_true + 0*np.random.randn(N)
+mu_est = mu_true + 1*np.random.randn(M,N)
+var_est = var_true + 0.01*np.random.randn(M,N)
+
+# Now simulate the Markov chain so that we get the true states and a measurement
+# for each state according to the distribtions
+T = 8000
+true_state_vec = np.zeros(T)
+est_state_vec = np.zeros(T)
+est_state_vec2 = np.zeros(T)
+observation_vec = np.zeros((M,T))
+
+bounds = np.array([0] + np.cumsum(pi_est).tolist())
+state_idx = np.arange(N)
+cur_state = -1
+
+for tt in range(T):
+    
+    rand_num = np.random.rand()
+    
+    if tt == 0:
+        bounds = np.array([0] + np.cumsum(pi_est).tolist())
+    else:
+        bounds = np.array([0] + np.cumsum(A_est[cur_state,:]).tolist())
+    
+    cur_state = state_idx[((bounds[:-1] < rand_num) & (rand_num <= bounds[1:]))][0]
+    
+    observation_vec[:,tt] = get_observation(mu_est[:,cur_state],var_est[:,cur_state])
+    true_state_vec[tt] = cur_state
+        
+# Now implement the forward/backward algorithm coupled with the baum-welch algorithm
+iters = 10
+for jj in range(iters):
+    alpha = np.zeros((N,T))
+    beta = np.zeros((N,T))
+    gamma = np.zeros((N,T))
+    little_b = np.zeros((N,T))
+    
+    for tt in range(T):
+        
+        # Compute likelihood
+        for ii in range(N):
+            t1 = ((2*np.pi)**(M/2.))*(np.prod(var_est[:,ii])**0.5)
+            t2 = np.sum(-((observation_vec[:,tt]-mu_est[:,ii])**2)/(2*var_est[:,ii]))
+            little_b[ii,tt] = np.exp(t2)/t1
+        little_b[:,tt] = little_b[:,tt]/little_b[:,tt].sum()
+        
+        # Compute forward terms
+        if tt == 0:
+                alpha[:,tt] = little_b[:,tt]*pi_est
+        else:
+            alpha[:,tt] = np.dot(alpha[:,tt-1].T,A_est)*little_b[:,tt]
+        alpha[:,tt] = alpha[:,tt]/np.sum(alpha[:,tt])
+    
+    for tt in range(T-1,-1,-1):
+        
+        # Compute beta
+        if tt == T-1:
+            beta[:,tt] = 1.
+        else:
+            beta[:,tt] = np.dot(A_est,beta[:,tt+1])*little_b[:,tt+1]
+            beta[:,tt] = beta[:,tt]/np.sum(beta[:,tt])
+        
+        # Compute gamma
+        gamma[:,tt] = alpha[:,tt]*beta[:,tt]
+        gamma[:,tt] = gamma[:,tt]/gamma[:,tt].sum()
+        
+        # Get current state estimate
+        est_state_vec[tt] = state_idx[gamma[:,tt] == gamma[:,tt].max()][0]
+        est_state_vec2[tt] = state_idx[little_b[:,tt] == little_b[:,tt].max()][0]
+        
+    print np.sum(true_state_vec-est_state_vec==0)/float(T)
+    print np.sum(true_state_vec-est_state_vec2==0)/float(T)
+    
+    tmp_mu = np.zeros((M,N))
+    tmp_var = np.zeros((M,N))
+    for ii in range(N):
+        gamma_mat = np.tile(gamma[ii,:],(M,1))
+        tmp_mu[:,ii]  = np.sum(gamma_mat*observation_vec,axis=1)/gamma[ii,:].sum()
+        tmp_var[:,ii] = np.sum(gamma_mat*(np.tile(tmp_mu[:,ii],(T,1)).T-observation_vec)**2,axis=1)/gamma[ii,:].sum()
+        
+#         little_b_mat = np.tile(little_b[ii,:],(M,1))
+#         tmp_mu[:,ii]  = np.sum(little_b_mat*observation_vec,axis=1)/little_b[ii,:].sum()
+#         tmp_var[:,ii] = np.sum(little_b_mat*(np.tile(tmp_mu[:,ii],(T,1)).T-observation_vec)**2,axis=1)/little_b[ii,:].sum()
+        
+        mu_est = tmp_mu.copy()
+        var_est = tmp_var.copy()
+        
+    print tmp_mu
+    print mu_true
+    print tmp_var
+    print var_true
--- two_state_example.py
+++ two_state_example.py
@@ -23,16 +23,36 @@ Created on Aug 25, 2016
 
 import numpy as np
 
+def get_gaus_prob(obs,dist_params):
+    mu  = dist_params[0]
+    var = dist_params[1]**2
+    t1 = 1/np.sqrt(2*np.pi*var)
+    t2 = (obs-mu)**2
+    t3 = (2*var)
+    return t1*np.exp(-t2/t3)
+    
+def my_round(val,decimal_place):
+    return np.round(val*10**decimal_place)/10**decimal_place
+
 # Set up the true Markov chain parameters and the true distribution parameters
-pi_true = np.array([0.99,0.01])
-A_true = np.array([[0.99,0.1],[0.5,0.5]])
-state1_dist_params = np.array([0.,np.sqrt(1.)])
-state2_dist_params = np.array([4.,np.sqrt(2.)])
+pi_true = np.array([0.90,0.10])
+A_true = np.array([[0.90,0.10],[0.10,0.90]])
+state1_dist_params = np.array([0.0,np.sqrt(1.0)])
+state2_dist_params = np.array([4.0,np.sqrt(2.0)])
+
+# Set up the guesses for the distributions for each state and the HMM model 
+# params
+pi_est = np.array([0.90,0.10])
+A_est = np.array([[0.90,0.10],[0.10,0.90]])
+est_state1_dist_params = np.array([-0.5,np.sqrt(0.8)])
+est_state2_dist_params = np.array([5.5,np.sqrt(2.3)])
 
 # Now simulate the Markov chain so that we get the true states and a measurement
 # for each state according to the distribtions
-num_time_steps = 10
+num_time_steps = 5000
 true_state_vec = np.zeros(num_time_steps)
+est_state_vec = np.zeros(num_time_steps)
+est_state_vec2 = np.zeros(num_time_steps)
 observation_vec = np.zeros(num_time_steps)
 
 for ii in range(num_time_steps):
@@ -66,4 +86,80 @@ for ii in range(num_time_steps):
     else:
         observation_vec[ii] = np.sum(state2_dist_params*rand_val)
 
+# Now run the forward/backward algorithm to get gamma for each time step
+iters = 100
+for jj in range(iters):
 
+    alpha = np.zeros((2,num_time_steps))
+    beta = np.zeros((2,num_time_steps))
+    gamma = np.zeros((2,num_time_steps))
+    little_b = np.zeros((2,num_time_steps))
+    
+    for ii in range(num_time_steps):
+        
+        little_b[0,ii] = get_gaus_prob(observation_vec[ii],est_state1_dist_params)
+        little_b[1,ii] = get_gaus_prob(observation_vec[ii],est_state2_dist_params)
+        little_b[:,ii] = little_b[:,ii]/little_b[:,ii].sum()
+        
+        if ii == 0:
+            alpha[:,ii] = little_b[:,ii]*pi_est
+        else:
+            alpha[:,ii] = np.dot(alpha[:,ii-1].T,A_est)*little_b[:,ii]
+    
+        alpha[:,ii] = alpha[:,ii]/np.sum(alpha[:,ii])
+    
+    for ii in range(num_time_steps-1,-1,-1):
+        
+        if ii == num_time_steps-1:
+            beta[:,ii] = 1.
+        else:
+            beta[:,ii] = np.dot(A_est,beta[:,ii+1])*little_b[:,ii+1]
+            beta[:,ii] = beta[:,ii]/np.sum(beta[:,ii])
+            
+        gamma[:,ii] = alpha[:,ii]*beta[:,ii]
+        gamma[:,ii] = gamma[:,ii]/gamma[:,ii].sum()
+        
+        if gamma[0,ii] > gamma[1,ii]:
+            est_state_vec[ii] = 1
+        else:
+            est_state_vec[ii] = 2
+            
+        if little_b[0,ii] > little_b[1,ii]:
+            est_state_vec2[ii] = 1
+        else:
+            est_state_vec2[ii] = 2
+                
+    print np.sum(true_state_vec-est_state_vec==0)/float(num_time_steps)
+    print np.sum(true_state_vec-est_state_vec2==0)/float(num_time_steps)
+    print np.argwhere(np.abs(true_state_vec-est_state_vec)==1).size
+        
+#     est_mu1 = np.sum(gamma[0,:]*observation_vec)/np.sum(gamma[0,:])
+#     est_mu2 = np.sum(gamma[1,:]*observation_vec)/np.sum(gamma[1,:])
+#      
+#     est_var1 = np.sum(gamma[0,:]*(observation_vec-est_mu1)**2)/(gamma[0,:].sum())
+#     est_var2 = np.sum(gamma[1,:]*(observation_vec-est_mu2)**2)/(gamma[1,:].sum())
+    
+    est_mu1 = np.sum(little_b[0,:]*observation_vec)/np.sum(little_b[0,:])
+    est_mu2 = np.sum(little_b[1,:]*observation_vec)/np.sum(little_b[1,:])
+     
+    est_var1 = np.sum(little_b[0,:]*(observation_vec-est_mu1)**2)/(little_b[0,:].sum())
+    est_var2 = np.sum(little_b[1,:]*(observation_vec-est_mu2)**2)/(little_b[1,:].sum())
+    
+    est_state1_dist_params = np.array([est_mu1,np.sqrt(est_var1)])
+    est_state2_dist_params = np.array([est_mu2,np.sqrt(est_var2)])
+    
+    print "---------------------------------------------------------"
+    print "          | True Mean | Est. Mean | True Var | Est. Var |"
+    print "---------------------------------------------------------"
+    line1  = "| State 1 |    " + str(state1_dist_params[0]) + "    |   "
+    line1 += str(my_round(est_mu1,3)) + "   |   "
+    line1 += str(state1_dist_params[1]**2) + "    |   "
+    line1 += str(my_round(est_var1, 3)) + "  |  "
+    print line1
+    print "---------------------------------------------------------"
+    line1  = "| State 2 |    " + str(state2_dist_params[0]) + "    |   "
+    line1 += str(my_round(est_mu2,3)) + "   |   "
+    line1 += str(state2_dist_params[1]**2) + "    |   "
+    line1 += str(my_round(est_var2, 3)) + "  |  "
+    print line1
+    print "---------------------------------------------------------\n"
